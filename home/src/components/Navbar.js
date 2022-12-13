c6fcac9a53edbcc845fb7886bdcb6a84e0dedf42
Roshila Hatharasinghe <73441371+RoshilaHatharasinghe@users.noreply.github.com>
2022-12-13 11:17:18 +0530

    Add files via upload

---
+++ home/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Navbar() {
+  return (
+    <>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+            <Link to="/" className='navbar-logo'></Link>
+                
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
