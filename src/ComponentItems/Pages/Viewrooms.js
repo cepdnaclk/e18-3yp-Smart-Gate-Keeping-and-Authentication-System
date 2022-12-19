8c07ff72a14e508f82b92a655a4836f9bc325b1e
HirushaUthsara <e18368@eng.pdn.ac.lk>
2022-12-19 11:24:59 +0530

    completed the view user and view rooms basic ui design

--- src/ComponentItems/Pages/Viewrooms.js
+++ src/ComponentItems/Pages/Viewrooms.js
@@ -1,5 +1,5 @@
 // import React from 'react';
-  
+
 // const Viewerooms = () => {
 //   return (
 //     <div
@@ -14,58 +14,74 @@
 //     </div>
 //   );
 // };
-  
 
 // export default Viewerooms;
+import React from "react";
+import { Button, Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+import "./Viewroom.css";
 
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { Container, Row, Col, Button, Table } from 'react-bootstrap';
-
-const Viewerooms = () => {
-  const { id } = useParams();
-  const room = {
-    id: id,
-    name: 'Meeting Room 1',
-    type: 'Pre-allocated',
-  };
+const Viewrooms = () => {
+  const rooms = [
+    {
+      id: 1,
+      name: "Room 1",
+      type: "Pre-allocated",
+    },
+    {
+      id: 2,
+      name: "Room 2",
+      type: "None-allocated",
+    },
+    // additional rooms go here
+  ];
 
   return (
-    <Container>
-      <Row className="mb-3">
-        <Col>
-          <h1>Room Details</h1>
-        </Col>
-      </Row>
+    <CSSTransition in={true} timeout={300} classNames="fade" unmountOnExit>
       <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Room ID</th>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
         <tbody>
-          <tr>
-            <td>ID</td>
-            <td>{room.id}</td>
-          </tr>
-          <tr>
-            <td>Name</td>
-            <td>{room.name}</td>
-          </tr>
-          <tr>
-            <td>Type</td>
-            <td>{room.type}</td>
-          </tr>
+          {rooms.map((room) => (
+            <tr key={room.id}>
+              <td>{room.id}</td>
+              <td>{room.name}</td>
+              <td>{room.type}</td>
+              <td>
+                <Link to="/create-slot">
+                  <Button variant="link" style={{ color: "black" }}>
+                    Create a Slot
+                  </Button>
+                </Link>
+                <Link to="/view-slots">
+                  <Button variant="link" style={{ color: "black" }}>
+                    View Slots
+                  </Button>
+                </Link>
+                <Link to="/view-users">
+                  <Button variant="link" style={{ color: "black" }}>
+                    View Users
+                  </Button>
+                </Link>
+                <Link to="/add-users">
+                  <Button variant="link" className="action-button">
+                    Add Users
+                  </Button>
+                </Link>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
-      <Row className="mt-5">
-        <Col>
-          <Button variant="primary">Create a Slot</Button>
-        </Col>
-        <Col>
-          <Button variant="secondary">View Slots</Button>
-        </Col>
-        <Col>
-          <Button variant="info">View Users</Button>
-        </Col>
-      </Row>
-    </Container>
+    </CSSTransition>
   );
 };
 
-export default Viewerooms;
+export default Viewrooms;
