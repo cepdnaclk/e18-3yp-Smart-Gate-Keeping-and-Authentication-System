8545fccda9be6ceee114c37d9479b072809a184d
Roshila Hatharasinghe <e18330@eng.pdn.ac.lk>
2022-12-19 15:26:22 +0530

    Signup testing added

---
+++ src/ComponentItems/__test__/Signup.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Signup from "../Sign/Signup";
+
+describe("Test the Signup component", () => {
+    test("render the signup form with 2 button", async () => {
+        render(<Signup />);
+        const buttonList = await screen.findAllByRole("button");
+        expect(buttonList).toHaveLength(1);
+    });
+});
