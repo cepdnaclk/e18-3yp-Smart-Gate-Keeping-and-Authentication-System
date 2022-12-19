e59432bcd0590d603f33ecb12cbc60cbdd195474
Roshila Hatharasinghe <e18330@eng.pdn.ac.lk>
2022-12-19 11:44:15 +0530

    Update CardItem.js

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -7,14 +7,14 @@ function CardItem(props) {
       <li className='cards__item'>
         <Link className='cards__item__link' to={props.path}>
           <figure className='cards__item__pic-wrap' data-category={props.label}>
-            <img
-              className='cards__item__img'
+            <img 
+              src={props.img}
               alt='Travel'
-              // src="../assets/images/img-7.jpg"
+              className='cards__item__img'
             />
           </figure>
           <div className='cards__item__info'>
-            <h2 className='cards__item__text'>{props.text}</h2>
+            <h5 className='cards__item__text'>{props.text}</h5>
           </div>
         </Link>
       </li>
