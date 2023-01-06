0d8accd68c94f20665b727b22dbb210a15dfbb99
HirushaUthsara <e18368@eng.pdn.ac.lk>
2023-01-06 11:48:33 +0530

    trash

--- NodeMCU ESP8266-12E/src/main.cpp
+++ NodeMCU ESP8266-12E/src/main.cpp
@@ -1,170 +1,42 @@
 #include <Arduino.h>
-
-#include <ESP8266WiFi.h>
-
-#include <HttpClient.h>
-
-#include <Firebase_ESP_Client.h>
-
-// Provide the token generation process info.
-#include <addons/TokenHelper.h>
-
-/* 1. Define the WiFi credentials */
-#define WIFI_SSID "Galaxy M02s5656"
-#define WIFI_PASSWORD "hiru2857756"
-
-/* 2. Define the API Key */
-#define API_KEY "AIzaSyAg72CAX2SWsYzmN0B9NxYAHnE_oFgijVU"
-
-/* 3. Define the project ID */
-#define FIREBASE_PROJECT_ID "co300project"
-
-/* 4. Define the user Email and password that alreadey registerd or added in your project */
-#define USER_EMAIL "e18368@eng.pdn.ac.lk"
-#define USER_PASSWORD "abcd1234"
-
-#define MOTORPIN 6
-
-// Define Firebase Data object
-FirebaseData fbdo;
-
-FirebaseAuth auth;
-FirebaseConfig config;
-
-unsigned long dataMillis = 0;
-int count = 0;
-
-const char hostAPI [] = "https://us-central1-co300project.cloudfunctions.net/app"; 
-HttpClient http;
-
-// Number of milliseconds to wait without receiving any data before we give up
-const int kNetworkTimeout = 30*1000;
-// Number of milliseconds to wait if no data is available before trying again
-const int kNetworkDelay = 1000;
+#include <Adafruit_Fingerprint.h>
 
 
-void setup()
-{
 
-    Serial.begin(115200);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial);
 
-    // set up pin for pwm to control servo motor
-    analogWrite(MOTORPIN,512);
 
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print("Connecting to Wi-Fi");
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.print(".");
-        delay(300);
-    }
-    Serial.println();
-    Serial.print("Connected with IP: ");
-    Serial.println(WiFi.localIP());
-    Serial.println();
-
-    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
-
-    /* Assign the project host and api key (required) */
-    config.api_key = API_KEY;
-
-    /* Assign the user sign in credentials */
-    auth.user.email = USER_EMAIL;
-    auth.user.password = USER_PASSWORD;
-
-    /* Assign the callback function for the long running token generation task */
-    config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
-
-#if defined(ESP8266)
-    // In ESP8266 required for BearSSL rx/tx buffer for large data handle, increase Rx size as needed.
-    fbdo.setBSSLBufferSize(2048 /* Rx buffer size in bytes from 512 - 16384 */, 2048 /* Tx buffer size in bytes from 512 - 16384 */);
-#endif
-
-    // Limit the size of response payload to be collected in FirebaseData
-    fbdo.setResponseSize(2048);
-
-    Firebase.begin(&config, &auth);
-
-    Firebase.reconnectWiFi(true);
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  finger.begin(57600);
 }
 
-void loop()
-{
-    // uint16_t dutyCycle = analogRead(A0);
-   int err =0;
-  
-  
-  err = http.get(hostAPI, "/api/100/checkroom/1");
-  if (err == 0)
-  {
-    Serial.println("startedRequest ok");
 
-    err = http.responseStatusCode();
-    if (err >= 0)
-    {
-      Serial.print("Got status code: ");
-      Serial.println(err);
 
-      // Usually you'd check that the response code is 200 or a
-      // similar "success" code (200-299) before carrying on,
-      // but we'll print out whatever response we get
 
-      err = http.skipResponseHeaders();
-      if (err >= 0)
-      {
-        int bodyLen = http.contentLength();
-        Serial.print("Content length is: ");
-        Serial.println(bodyLen);
-        Serial.println();
-        Serial.println("Body returned follows:");
-      
-        // Now we've got to the body, so we can print it out
-        unsigned long timeoutStart = millis();
-        char c;
-        // Whilst we haven't timed out & haven't reached the end of the body
-        while ( (http.connected() || http.available()) &&
-               ((millis() - timeoutStart) < kNetworkTimeout) )
-        {
-            if (http.available())
-            {
-                c = http.read();
-                // Print out this character
-                Serial.print(c);
-               
-                bodyLen--;
-                // We read something, reset the timeout counter
-                timeoutStart = millis();
-            }
-            else
-            {
-                // We haven't got any data, so let's pause to allow some to
-                // arrive
-                delay(kNetworkDelay);
-            }
-        }
-      }
-      else
-      {
-        Serial.print("Failed to skip response headers: ");
-        Serial.println(err);
-      }
-    }
-    else
-    {    
-      Serial.print("Getting response failed: ");
-      Serial.println(err);
+void loop() {
+  // Check if a finger is placed on the sensor
+  uint8_t p = finger.getImage();
+  if (p != FINGERPRINT_OK) return;
+
+  // Image taken, convert it to feature template
+  p = finger.image2Tz();
+  if (p != FINGERPRINT_OK) return;
+
+  // Check if the user wants to register a new fingerprint
+  if (Serial.available() && Serial.read() == 'r') {
+    // Add the fingerprint template to the database
+    p = finger.storeModel(1);
+    if (p != FINGERPRINT_OK) return;
+    Serial.println("Fingerprint registered!");
+  } else {
+    // Identify the fingerprint against the database
+    uint8_t id = finger.fingerFastSearch();
+    if (id < 256) {
+      Serial.println("Fingerprint recognized, ID: " + String(id));
+    } else {
+      Serial.println("Fingerprint not recognized");
     }
   }
-  else
-  {
-    Serial.print("Connect failed: ");
-    Serial.println(err);
-  }
-  http.stop();
-
-  // And just stop, now that we've tried a download
-  while(1);
-    
-
-    
 }
