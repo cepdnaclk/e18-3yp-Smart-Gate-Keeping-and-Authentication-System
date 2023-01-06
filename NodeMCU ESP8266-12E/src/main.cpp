ff3c116562914483ae889218f764029684bf6839
HirushaUthsara <e18368@eng.pdn.ac.lk>
2023-01-06 09:27:06 +0530

    new update

--- NodeMCU ESP8266-12E/src/main.cpp
+++ NodeMCU ESP8266-12E/src/main.cpp
@@ -2,6 +2,8 @@
 
 #include <ESP8266WiFi.h>
 
+#include <HttpClient.h>
+
 #include <Firebase_ESP_Client.h>
 
 // Provide the token generation process info.
@@ -31,6 +33,15 @@ FirebaseConfig config;
 
 unsigned long dataMillis = 0;
 int count = 0;
+
+const char hostAPI [] = "https://us-central1-co300project.cloudfunctions.net/app"; 
+HttpClient http;
+
+// Number of milliseconds to wait without receiving any data before we give up
+const int kNetworkTimeout = 30*1000;
+// Number of milliseconds to wait if no data is available before trying again
+const int kNetworkDelay = 1000;
+
 
 void setup()
 {
@@ -79,7 +90,81 @@ void setup()
 
 void loop()
 {
-    uint16_t dutyCycle = analogRead(A0);
+    // uint16_t dutyCycle = analogRead(A0);
+   int err =0;
+  
+  
+  err = http.get(hostAPI, "/api/100/checkroom/1");
+  if (err == 0)
+  {
+    Serial.println("startedRequest ok");
+
+    err = http.responseStatusCode();
+    if (err >= 0)
+    {
+      Serial.print("Got status code: ");
+      Serial.println(err);
+
+      // Usually you'd check that the response code is 200 or a
+      // similar "success" code (200-299) before carrying on,
+      // but we'll print out whatever response we get
+
+      err = http.skipResponseHeaders();
+      if (err >= 0)
+      {
+        int bodyLen = http.contentLength();
+        Serial.print("Content length is: ");
+        Serial.println(bodyLen);
+        Serial.println();
+        Serial.println("Body returned follows:");
+      
+        // Now we've got to the body, so we can print it out
+        unsigned long timeoutStart = millis();
+        char c;
+        // Whilst we haven't timed out & haven't reached the end of the body
+        while ( (http.connected() || http.available()) &&
+               ((millis() - timeoutStart) < kNetworkTimeout) )
+        {
+            if (http.available())
+            {
+                c = http.read();
+                // Print out this character
+                Serial.print(c);
+               
+                bodyLen--;
+                // We read something, reset the timeout counter
+                timeoutStart = millis();
+            }
+            else
+            {
+                // We haven't got any data, so let's pause to allow some to
+                // arrive
+                delay(kNetworkDelay);
+            }
+        }
+      }
+      else
+      {
+        Serial.print("Failed to skip response headers: ");
+        Serial.println(err);
+      }
+    }
+    else
+    {    
+      Serial.print("Getting response failed: ");
+      Serial.println(err);
+    }
+  }
+  else
+  {
+    Serial.print("Connect failed: ");
+    Serial.println(err);
+  }
+  http.stop();
+
+  // And just stop, now that we've tried a download
+  while(1);
     
+
     
 }
