ae4fccb4176e4b5b43a7cd74ce0a8cc1ec03520a
HirushaUthsara <e18368@eng.pdn.ac.lk>
2023-01-08 17:28:58 +0530

    added the text and char display functionality

--- NodeMCU ESP12E/src/main.cpp
+++ NodeMCU ESP12E/src/main.cpp
@@ -1,107 +1,72 @@
 #include <Arduino.h>
 #include <Adafruit_Fingerprint.h>
 #include <SoftwareSerial.h>
+#include <LiquidCrystal_I2C.h>
+
+// set the LCD number of columns and rows
+#define LCDColumns 16
+#define LCDRow 2
+
+LiquidCrystal_I2C lcd(0x27, LCDColumns, LCDRow);
 
 SoftwareSerial mySerial(13, 15);
-Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial); // fingerprint controller variable
+
+// function prototypes
+uint8_t genarateNewID();
+uint16_t registerFingerprint();
+void displayText(String text, uint8_t cursor1, uint8_t cursor2);
 
 void setup()
 {
   Serial.begin(115200);
-  delay(100);
-  Serial.println("\n\nFingerprint sensor enrollment");
+  /* Set up for the LCD 16x2 display */
+  Wire.begin();
+  Serial.println("\nI2C Scanner");
 
-  finger.begin(57600); // set the data rate for the sensor serial port
+  /* Set up for the Fingerprint sensor */
 
-  if (finger.verifyPassword())
-  {
-    Serial.println("Found fingerprint sensor!");
-  }
-  else
-  {
-    Serial.println("Did not find fingerprint sensor :(");
-    while (1)
-    {
-      delay(1);
-    }
-  }
+  // Serial.println("\n\nFingerprint sensor enrollment");
+
+  // finger.begin(57600); // set the data rate for the sensor serial port
+
+  // if (finger.verifyPassword()) // check sensor is active and responding
+  // {
+  //   Serial.println("Found fingerprint sensor!");
+  // }
+  // else
+  // {
+  //   Serial.println("Did not find fingerprint sensor :------------");
+  //   while (1)
+  //   {
+  //     delay(1);
+  //   }
+  // }
 }
 
 void loop()
 {
-  int id = 1;
-  Serial.println("Place finger on sensor to register");
-  int p = -1;
+  // int newID = genarateNewID();
+  // Serial.println(newID);
+  // displayText("Hello World!!",0,0);
+}
 
-  Serial.print("Waiting for valid finger to enroll as #");
-  while (p != FINGERPRINT_OK)
-  {
-    p = finger.getImage();
-    switch (p)
-    {
-    case FINGERPRINT_OK:
-      Serial.println("Image taken");
-      break;
-    case FINGERPRINT_NOFINGER:
-      Serial.println(".");
-      break;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      break;
-    case FINGERPRINT_IMAGEFAIL:
-      Serial.println("Imaging error");
-      break;
-    default:
-      Serial.println("Unknown error");
-      break;
-    }
+uint8_t genarateNewID()
+{
+  int count = finger.getTemplateCount(); // get the no of fingerprint templates on memory
+  if (count != FINGERPRINT_OK)
+    return -1;
+  return count + 1;
+}
 
-    p = finger.image2Tz(2);
-    switch (p)
-    {
-    case FINGERPRINT_OK:
-      Serial.println("Image converted");
-      break;
-    case FINGERPRINT_IMAGEMESS:
-      Serial.println("Image too messy");
-      break;
-    case FINGERPRINT_PACKETRECIEVEERR:
-      Serial.println("Communication error");
-      break;
-    case FINGERPRINT_FEATUREFAIL:
-      Serial.println("Could not find fingerprint features");
-      break;
-    case FINGERPRINT_INVALIDIMAGE:
-      Serial.println("Could not find fingerprint features");
-      break;
-    default:
-      Serial.println("Unknown error");
-      break;
-    }
+// uint16_t registerFingerprint(){
+//   Serial.println("Place finger on sensor to register");
+//   uint16_t id = genarateNewID();         // get the next id to store
 
-    Serial.print("Creating model for #");
-    Serial.println(id);
-    p = finger.createModel();
-    if (p == FINGERPRINT_OK)
-    {
-      Serial.println("Prints matched!");
-    }
-    else if (p == FINGERPRINT_PACKETRECIEVEERR)
-    {
-      Serial.println("Communication error");
-      // return p;
-    }
-    else if (p == FINGERPRINT_ENROLLMISMATCH)
-    {
-      Serial.println("Fingerprints did not match");
-      // return p;
-    }
-    else
-    {
-      Serial.println("Unknown error");
-      // return p;
-    }
+// }
 
-    p = finger.storeModel(id);
-  }
+void displayText(String text, uint8_t cursor1, uint8_t cursor2)
+{
+  lcd.setCursor(cursor1, cursor2); // set the cursor position
+  lcd.print(text);                 // print the text
 }
